99806c6af0bcbf5cc5575133bb0b13b20dc3992b
Rachael Blake <blake@aurora>
2017-05-16 18:30:21 -0700

    ran NMDS; plotted NMDS

--- NMDS_analysis.r
+++ NMDS_analysis.r
@@ -23,30 +23,33 @@ source("PDO_cleaning.r")      # dataframes are pdo_mon and pdo_ann
 # add in the PDO data
 PerCov_PDO <- PerCov_clean %>%
               dplyr::left_join(pdo_ann, by="Year") %>%
-# and subset to just the control treatment
               dplyr::filter(Treatment == "01") %>%
               dplyr::select(-standard_code, -plot, -abbr_code, -Treatment, -Block) %>%
+              #dplyr::select(-standard_code, -plot, -abbr_code, -Block) %>%
               dplyr::group_by(Year) %>%
+              #dplyr::group_by(Year, Treatment) %>%
               dplyr::summarize_each(funs(mean)) %>%
-              dplyr::ungroup()
+              dplyr::ungroup() %>%
+              dplyr::mutate(PDO_Sign = ifelse(PDO_anul_mn>0, "A", "B"))
 
 
 # subset data into seperate dataframes
 sp_percov <- PerCov_PDO %>% dplyr::select(FUCUS_TOTAL, Barnacles, Mytilus, Pterosiphonia_poly,
                                           Odonthalia, Barnacle_spat, Endocladia, FUCUS_SPORELINGS,
                                           Clad_sericia, Masto_pap, Gloiopeltis, Elachista)
-yr_percov <- PerCov_PDO[,"Year"]
-pdo_percov <- PerCov_PDO[,"PDO_anul_mn"]
+yr_percov <- PerCov_PDO$Year
+pdo_percov <- PerCov_PDO$PDO_anul_mn
+pdo_sign <- PerCov_PDO$PDO_Sign
 
 # do the analysis
 percov_mds <- vegan::metaMDS(sp_percov, distance="bray", k=2, trymax=1000, autotransform=TRUE)
 
+# plot it
+plot(percov_mds$points, col=as.factor(pdo_sign), pch=16, cex=0.9) 
 
 
 
-
-
-
+#######################################################################
 # making example dataset for posting question to StackOverflow
 df_species <- sp_percov %>%
               dplyr::rename(Species_1 = FUCUS_TOTAL,
