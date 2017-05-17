9f2ea211389f037a39f44d587128e4b5f1880df3
Rachael Blake <blake@aurora>
2017-05-17 16:56:29 -0700

    added freshwater discharge cleaning script

---
+++ Fresh_Discharge_cleaning.r
@@ -0,0 +1,111 @@
+#################################################################
+#####  Terrie Klinger's Kasitsna Bay Data                   #####
+#####  Hill mode freshwater discharge data cleaning script  #####
+#####  by Rachael E. Blake                                  #####
+#####  17 May 2017                                          #####
+#################################################################
+
+### load necessary packages
+library(ncdf4) ; library(chron)
+
+### read in the netcdf file and parse and clean the parts
+nc_kbay <- nc_open("GOA_RUNOFF_DISCHARGE.ncml.nc")
+#print(nc_kbay)
+
+# get time 
+time_date <- ncvar_get(nc_kbay, "time")
+time_date_units <- ncatt_get(nc_kbay, "time", "units")
+
+# get discharge
+mean_daily_discharge_m3s1 <- ncvar_get(nc_kbay, "q")
+discharge_lname <- ncatt_get(nc_kbay, "q", "long_name")
+discharge_units <- ncatt_get(nc_kbay, "q", "units")
+discharge_fillvalue <- ncatt_get(nc_kbay, "q", "_FillValue")
+
+# get latitude
+latitude_deg_north <- ncvar_get(nc_kbay, "lat")
+lat_lname <- ncatt_get(nc_kbay, "lat", "long_name")
+lat_units <- ncatt_get(nc_kbay, "lat", "units")
+
+# get longitude
+longitude_deg_east <- ncvar_get(nc_kbay, "lon")
+lon_lname <- ncatt_get(nc_kbay, "lon", "long_name")
+lon_units <- ncatt_get(nc_kbay, "lon", "units")
+
+# get general attributes
+title <- ncatt_get(nc_kbay, 0, "title")
+summary <- ncatt_get(nc_kbay, 0, "summary")
+institution <- ncatt_get(nc_kbay, 0, "institution")
+history <- ncatt_get(nc_kbay, 0, "history")
+Conventions <- ncatt_get(nc_kbay, 0, "Conventions")
+
+# close the connection to the netcdf file
+nc_close(nc_kbay)
+
+# convert time variable to human-readable format
+time_unit_str <- strsplit(time_date_units$value, " ")
+date_str <- strsplit(unlist(time_unit_str)[3], "-")
+start_month <- as.integer(unlist(date_str)[2])
+start_day <- as.integer(unlist(date_str)[3])
+start_year <- as.integer(unlist(date_str)[1]) 
+
+# make date column   # NOTE Each array slice is a date!!
+date <- chron(time_date, origin=c(start_month, start_day, start_year))
+
+# replace FillValue with NA
+mean_daily_discharge_m3s1[is.nan(mean_daily_discharge_m3s1)] <- NA
+
+### make a lat lon dataframe
+latlon <- as.data.frame(matrix(ncol=0, nrow=108))
+latlon$latitude_deg_north <- as.vector(latitude_deg_north)
+latlon$longitude_deg_east <- as.vector(longitude_deg_east)
+
+### Function to create a giant dataframe from slices of the netcdf file
+
+slice_2_df <- function(slice){
+              # make empty dataframe
+              df1 <- as.data.frame(matrix(ncol=0, nrow=108))
+              # make date column
+              df1$date <- as.character(date[[slice]])
+              # make data column
+              df1$mean_daily_discharge_m3s1 <- as.vector(mean_daily_discharge_m3s1[,,slice])
+  
+              lldf1 <- cbind(latlon, df1)
+              lldf1 <- dplyr::filter(lldf1, !is.na(mean_daily_discharge_m3s1))
+  
+              return(lldf1)
+}
+
+
+num_slices <- c(1:5722)   # this is the known nmber of slices in the original array
+
+FWD_list <- lapply(num_slices, function(x) slice_2_df(x))
+
+FWD <- bind_rows(FWD_list)
+
+# remove some points that probably don't drain into this site
+FWD_less <- FWD %>% 
+            filter(!(longitude_deg_east < -151.55 & latitude_deg_north < 59.48),
+                   !(latitude_deg_north > 59.51 & longitude_deg_east > -151.45))
+
+# check to see the data are approx correct by plotting the lats and lons
+q <- qplot(data=FWD_less, x=longitude_deg_east, y=latitude_deg_north)
+
+# create annual means
+FWD_ann_mn <- FWD_less %>%
+  
+  
+  
+  
+  
+              mutate(Year = strsplit(unlist(date)[3], "/"))
+  
+  
+
+
+
+
+
+
+
+
